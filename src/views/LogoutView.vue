1486ef793e00fe8a6c52aded66ff637759bbba27
Álvaro Beltrán Santiago <alvarobeltransantiago@gmail.com>
2025-05-18 22:39:58 +0200

    little fix on logout

--- src/views/LogoutView.vue
+++ src/views/LogoutView.vue
@@ -12,7 +12,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="d-flex justify-content-center align-items-center bg-light">
+  <div class="d-flex justify-content-center align-items-center vh-100 bg-light">
     <div class="bg-white shadow rounded p-5 text-center" style="max-width: 400px; width: 100%;">
       <h1 class="text-primary mb-3">¡Hasta pronto!</h1>
       <p class="mb-4 fs-5 text-primary">Gracias por visitarnos. Redirigiendo a la página principal...</p>
